bbd8382e0349620be1d05b3963fed5beafc82e14
Cyberoiide <clement.bosle.dev@gmail.com>
2026-03-17 01:06:44 +0100

    refactor: remove some padding and shit so its kinda sync with the bottom layer

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -7,10 +7,10 @@ import {
 
 export default function NavBar() {
   return (
-    <NavigationMenu className="w-full mx-auto flex max-w-4xl items-center justify-between px-8 py-8 md:px-16">
+    <NavigationMenu className="w-full mx-auto flex max-w-4xl items-center justify-between py-8">
       <NavigationMenuList className="flex items-center gap-8 text-foreground text-sm font-medium uppercase tracking-wide no-underline">
         <NavigationMenuItem>
-          <NavigationMenuLink className="text-foreground font-bold hover:underline mr-80" href="/">Clément Bosle</NavigationMenuLink>
+          <NavigationMenuLink className="text-foreground font-bold hover:underline mr-115" href="/">Clément Bosle</NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink href="/about">About</NavigationMenuLink>
